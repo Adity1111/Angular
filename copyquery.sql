8ccef4d1f2bbba7a5a57fa24c630f02bb47742a5
Adity1111 <81062648+Adity1111@users.noreply.github.com>
2024-07-16 15:22:38 +0530

    Add files via upload

---
+++ copyquery.sql
@@ -0,0 +1,14 @@
+select First_name ,last_name from customer_table
+
+select DISTINCT first_name , last_name from customer_table;
+select * from persons; 
+
+select  firstname from persons where age=24;
+
+
+ALTER TABLE science_class ADD PRIMARY KEY (enrollment_no) ;
+
+insert into persons values( 4, 'aditi','rai' ,24) ,(5,'adity' ,'raj' ,31);
+
+copy persons from 'C:\Program Files\PostgreSQL\14\pgAdmin 4\docs\srtext.txt' delimiter',';
+
---
+++ sql alter.sql
@@ -0,0 +1,16 @@
+update  customer_table set age=55 where age=25;
+
+select * from science_class order by enrollment_no ASC
+//SQL adds an "Email" column to the "Person" table
+ALTER TABLE persons  
+ADD Email varchar(255)
+select * from persons
+
+Update science_class set science_marks =45 where name ='Popeye' ;
+
+Update science_class set name = 'Robb' where enrollment_no= 2;
+ delete from science_class WHERE name = 'Robb';
+ 
+ALTER TABLE SCIENCE_CLASS
+RENAME COLUMN Name to student_name;
+
